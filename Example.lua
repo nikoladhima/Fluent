47cd2309a0564bd7f7ecf3d15ed9341d6b7eb061
nikoladhima <151961875+nikoladhima@users.noreply.github.com>
2024-10-06 23:19:37 +0300

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -4,7 +4,7 @@ local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.
 
 local Window = Fluent:CreateWindow({
     Title = "Fluent " .. Fluent.Version,
-    SubTitle = "by nikoleto bonnie",
+    SubTitle = "by nikoleto",
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 460),
     Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
